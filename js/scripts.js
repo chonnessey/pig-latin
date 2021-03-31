996eede18ca82537f8f4113f6e3e41b611b83ca3
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-03-31 16:30:35 -0700

    WIP: add more js to file directory
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,24 @@
 const vowel = ["a", "e", "i", "o", "u"];
 
 function pigLatin(letter) {
-    // if (letter.includes(vowel)) {
     if (vowel.includes(letter)) {
       return true; 
     } else {
       return false;
     } 
 }
+
+function pigLatin(word) {
+    if (vowel.includes(word)) {
+      word.concat("way");
+      return true;
+    } else {
+      return false;
+    }
+}
+
+
+
 
 // UI logic
 
